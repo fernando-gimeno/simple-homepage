7e604ca0bd41c98a799f190779c0726f21b08d3e
Fernando Gimeno <fernandogimeno.ief@gmail.com>
2024-06-19 15:11:25 -0300

    Update theme toggle functionality and navigation menu styling

--- js/script.js
+++ js/script.js
@@ -1,18 +1,43 @@
 const toggleTheme = document.querySelector("#toggle-theme");
 const logo = document.querySelector(".logo");
+const menu = document.querySelector(".menu");
+const nav = document.querySelector(".nav");
+const closeBtn = document.querySelector(".close-btn");
 
 toggleTheme.addEventListener("change", () => {
   if (toggleTheme.checked) {
-    toggleTheme.parentElement.children[1].classList.remove("selected")
-    toggleTheme.parentElement.children[0].classList.add("selected")
-    toggleTheme.parentElement.children[0].setAttribute("src", "./images/Moon_fill.svg")
-    logo.setAttribute("src", "./images/alarado-icon-homepage-dark.svg")
+    toggleTheme.parentElement.children[1].classList.remove("selected");
+    toggleTheme.parentElement.children[0].classList.add("selected");
+    toggleTheme.parentElement.children[0].setAttribute(
+      "src",
+      "./images/Moon_fill.svg"
+    );
+    toggleTheme.parentElement.children[1].setAttribute(
+      "src",
+      "./images/Sun_fill_light.svg"
+    );
+    logo.setAttribute("src", "./images/alarado-icon-homepage-dark.svg");
     document.documentElement.setAttribute("data-theme", "dark");
   } else {
-    toggleTheme.parentElement.children[0].classList.remove("selected")
-    toggleTheme.parentElement.children[1].classList.add("selected")
-    toggleTheme.parentElement.children[0].setAttribute("src", "./images/Moon_fill.svg")
-    logo.setAttribute("src", "./images/alarado-icon-homepage.svg")
+    toggleTheme.parentElement.children[0].classList.remove("selected");
+    toggleTheme.parentElement.children[1].classList.add("selected");
+    toggleTheme.parentElement.children[0].setAttribute(
+      "src",
+      "./images/Moon_fill_light.svg"
+    );
+    toggleTheme.parentElement.children[1].setAttribute(
+      "src",
+      "./images/Sun_fill.svg"
+    );
+    logo.setAttribute("src", "./images/alarado-icon-homepage.svg");
     document.documentElement.setAttribute("data-theme", "light");
   }
+});
+
+menu.addEventListener("click", () => {
+  nav.classList.toggle("show")
+});
+
+closeBtn.addEventListener("click", () => {
+  nav.classList.remove("show")
 });
